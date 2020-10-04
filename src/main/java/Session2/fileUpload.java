43245bf0ecc3895d6bf351701412d85a0e148e74
SaroshhasanZia <sarosh_26@yahoo.com>
2020-10-04 20:19:35 +0500

    Session 2

---
+++ src/main/java/Session2/fileUpload.java
@@ -0,0 +1,41 @@
+package Session2;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.LocalFileDetector;
+
+import java.io.File;
+
+import static java.lang.Thread.sleep;
+
+public class fileUpload {
+
+    public static void main(String[] args)  {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\VenD\\Desktop\\chromedriver.exe");
+        System.setProperty( ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/test/upload/");
+        driver.manage().window().maximize();
+
+
+//        driver.setFileDetector(new LocalFileDetector());
+//        File file = new File(  "C:\\Users\\VenD\\IdeaProjects\\file");
+//        String imagePath = file.getAbsolutePath();
+//        //driver.setFileDetector(new LocalFileDetector());
+
+        driver.findElement( By.id( "uploadfile_0" ) ).sendKeys( "C:\\Users\\VenD\\Desktop\\FileIpload.txt");
+        driver.findElement( By.id( "terms" ) ).click();
+        driver.findElement( By.id( "submitbutton" ) ).click();
+        driver.quit();
+
+
+
+
+
+    }
+
+}
